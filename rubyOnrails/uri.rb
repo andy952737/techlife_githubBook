0bfa139ec213ce49055ab16c3d0d849791e8ca7f
Andy <andy952737@gmail.com>
2020-11-18 10:00:09 +0800

    File open

--- rubyOnrails/uri.rb
+++ rubyOnrails/uri.rb
@@ -9,5 +9,11 @@ File.open('filename', 'wb') do |file|
 end
 # https://aws.amazon.com/tw/blogs/developer/downloading-objects-from-amazon-s3-using-the-aws-sdk-for-ruby/
 
-
-
+ 
+#ruby for linux
+system "ls -la"
+system "mv"
+system "rm -rf" 
+system "mkdir -R"
+FileUtils.rm_rf(tmp_path) if File.directory?(tmp_path)
+FileUtils.mkdir_p(tmp_path) 
